b1bf1ae125e5c025d81fb9871f05fade1527c1e5
unknown <ale.crippa96@gmail.com>
2020-03-12 16:58:27 +0000

    chabges

--- proj.R
+++ proj.R
@@ -27,7 +27,7 @@ predM=init$predictorMatrix
 #meth[c("glucose","pressure","triceps", "insulin","mass")] <-"norm"
 
 # run the multiple imputation
-set.seed(101)
+set.seed(123)
 imputed = mice(Diab2, method=meth, predictorMatrix=predM, m=5)   # Q: is better to use pmm or norm method?
 imput<-complete(imputed)
 
@@ -40,7 +40,7 @@ test.set <- subset(imput, sample==FALSE)
 
 
 #############################################################
-###################       KNN       #########################
+###################       KNN       #########################bufera
 #############################################################
 
 train.x <- train.set[,1:8]
@@ -69,7 +69,7 @@ for (i in seq(along=ks)){
   
 }
 
-x11()
+#x11()
 plot(misclass.train,xlab="Number of NN",ylab="Test error",type="n",xaxt="n",ylim=c(0,0.4)) #type="n" non disegna nulla, xaxt="n" non mette valori sull'asse x
 axis(1, 1:length(ks), as.character(ks)) 
 lines(misclass.test,type="b",col='blue',pch=20)   
@@ -84,30 +84,82 @@ title('Test performace vs Train performace')
 
 col1<-c('blue', 'magenta')
 
-library(grid)  #ci serve per plottare una griglia
+library(grid)  
 plot.knn <- function(kneighbours){
   grid.x2 <- seq(min(imput[,2]), max(imput[,2]), 1)
-  grid.x8 <- seq(min(imput[,8]), max(imput[,8]), 1)
-  grid <- expand.grid(0, grid.x2,0,0,0,0,0,grid.x8)
+  grid.x6 <- seq(min(imput[,6]), max(imput[,6]), 1)
+  grid <- expand.grid(mean(imput[,1]),grid.x2,mean(imput[,3]),mean(imput[,4]),mean(imput[,5]),grid.x6,mean(imput[,7]),mean(imput[,8]))
   colnames(grid) <- colnames(train.x)
   predicted.classes <- knn(train.x, grid, classes, k=kneighbours)
   x11()
-  plot(imput$glucose, imput$age, pch=20, col=col1[as.numeric(imput$diabetes)], xlab='glucose', ylab='age')
-  points(grid$glucose, grid$age, pch='.', col=col1[as.numeric(predicted.classes)])  # draw grid
+  plot(imput$glucose, imput$mass, pch=20, col=col1[as.numeric(imput$diabetes)], xlab='glucose', ylab='mass')
+  points(grid$glucose, grid$mass, pch='.', col=col1[as.numeric(predicted.classes)])  # draw grid
   legend("topleft", legend=levels(imput$diabetes),fill =col1)
   title(c("Classification with k=", kneighbours))
 
-  predicted.matrix <- matrix(as.numeric(predicted.classes), length(grid.x2), length(grid.x8))
-  contour(grid.x2, grid.x8, predicted.matrix, levels=c(1.5), drawlabels=FALSE,add=TRUE)
+  predicted.matrix <- matrix(as.numeric(predicted.classes), length(grid.x2), length(grid.x6))
+  contour(grid.x2, grid.x6, predicted.matrix, levels=c(1.5), drawlabels=FALSE,add=TRUE)
   }
 
 plot.knn(1)
 plot.knn(3)
 plot.knn(5)
-plot.knn(10)
+plot.knn(8)
+
+
+##########################################################################################
+
+#                                  Linear regression
+
+##########################################################################################
+
+imput.x<-imput[,-9]
+
+ml<-lm(as.numeric(imput$diabetes)~pregnant+glucose+pressure+triceps+insulin+mass+pedigree+age, data=imput[,-9])
+summary(ml)
+ml<-lm(as.numeric(imput$diabetes)~pregnant+glucose+ pressure+  insulin+mass+pedigree+age, data=imput[,-9])
+summary(ml)
+ml<-lm(as.numeric(imput$diabetes)~pregnant+glucose+ pressure+ mass+pedigree+age, data=imput[,-9])
+summary(ml)
+ml<-lm(as.numeric(imput$diabetes)~pregnant+glucose+ mass+pedigree+age, data=imput[,-9])
+summary(ml)
+ml<-lm(as.numeric(imput$diabetes)~pregnant+glucose+ mass+pedigree, data=imput[,-9])
+summary(ml)
+
+# classifier
+
+f_col <- function(x){ if (x<1.5) 'blue' else 'magenta'}
+
+lsm <- function(regmod, data){
+  val <- round(predict(regmod, data))
+  pr.cl<-sapply(val,f_col)
+  return 
+}
+
+# Boundaries
+
+grid.x2 <- seq(min(imput[,2]), max(imput[,2]), 1)
+grid.x6 <- seq(min(imput[,6]), max(imput[,6]), 1)
+grid <- expand.grid(mean(imput[,1]),grid.x2,grid.x6,mean(imput[,7]))
+colnames(grid) <- colnames(train.x[c(1,2,6,7)])
+
+predicted.classes <- lsm(ml,grid)
+pr.cl.val <- as.numeric(data.frame(predicted.classes)$predicted.classes)
+
+x11()
+plot(imput$glucose, imput$mass, pch=20, col=col1[as.numeric(imput$diabetes)], xlab='glucose', ylab='mass')
+points(grid$glucose, grid$mass, pch='.', col=predicted.classes)  # draw grid
+legend("topleft", legend=levels(imput$diabetes),fill =col1)
+title(c("Classification with linear regression"))
+
+predicted.matrix <- matrix(pr.cl.val, length(grid.x2), length(grid.x6))
+contour(grid.x2, grid.x6, predicted.matrix, levels=c(1.5), drawlabels=FALSE,add=TRUE) # but it would be more reasonable draw a linear function
+
 
 ####################################################
+
 # PCA
+
 
 #install.packages("ggfortify")
 library(ggfortify)
@@ -116,7 +168,7 @@ install_github("vqv/ggbiplot")
 
 pca<- prcomp(imput[,-9], center = TRUE,scale. = TRUE)
 pca.values <- pca$x
-#pca <- princomp (train.x, cor = TRUE, scores = TRUE)
+pca <- princomp (train.x, cor = TRUE, scores = TRUE)
 str(pca)
 
 test.pred <- cbind(test.x, predicted.classes)
@@ -140,3 +192,65 @@ plot.knn <- function(kneighbours){
   predicted.matrix <- matrix(as.numeric(predicted.classes), length(grid.x2), length(grid.x8))
   contour(grid.x2, grid.x8, predicted.matrix, levels=c(1.5), drawlabels=FALSE,add=TRUE)
 }
+
+
+
+###########
+
+# CART
+
+install.packages('tidyverse')
+install.packages('caret')
+install.packages('rpart')
+
+library(tidyverse)
+library(caret)
+library(rpart)
+
+# Build the model
+model1 <- rpart(diabetes ~., data = train.set, method = "class")
+
+# Plot the trees
+par(xpd = NA) # Avoid clipping the text in some device
+plot(model1)
+text(model1, digits = 3)
+
+# Make predictions on the test data
+predicted.classes <- model1 %>% 
+  predict(test.set, type = "class")
+head(predicted.classes)
+
+# Compute model accuracy rate on test data
+mean(predicted.classes == test.set$diabetes)
+
+
+### Now prune the tree
+install.packages("e1071")
+
+# Fit the model on the training set
+set.seed(123)
+model2 <- train(
+  diabetes ~., data = train.set, method = "rpart",
+  trControl = trainControl("cv", number = 10),
+  tuneLength = 20
+)
+# Plot model accuracy vs different values of
+# cp (complexity parameter)
+plot(model2)
+
+# Print the best tuning parameter cp that
+# maximizes the model accuracy
+model2$bestTune
+
+# Plot the final tree model
+par(xpd = NA) # Avoid clipping the text in some device
+plot(model2$finalModel)
+text(model2$finalModel,  digits = 3)
+
+# Decision rules in the model
+model2$finalModel
+
+# Make predictions on the test data
+predicted.classes <- model2 %>% predict(test.set)
+# Compute model accuracy rate on test data
+mean(predicted.classes == test.set$diabetes)
