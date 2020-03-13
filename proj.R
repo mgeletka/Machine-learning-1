c3f24afb9c5b89b436b56196c4f657fcf0a46c56
unknown <ale.crippa96@gmail.com>
2020-03-13 16:35:08 +0000

    linear regression with boundary

--- proj.R
+++ proj.R
@@ -8,7 +8,11 @@ utils::data(PimaIndiansDiabetes2)
 Diab2<-PimaIndiansDiabetes2;
 
 
-###################################################################
+#####################################################################################################
+
+#                                         Preparing the dataset
+
+#####################################################################################################
 
 # MICE
 
@@ -27,7 +31,7 @@ predM=init$predictorMatrix
 #meth[c("glucose","pressure","triceps", "insulin","mass")] <-"norm"
 
 # run the multiple imputation
-set.seed(123)
+set.seed(101)
 imputed = mice(Diab2, method=meth, predictorMatrix=predM, m=5)   # Q: is better to use pmm or norm method?
 imput<-complete(imputed)
 
@@ -38,17 +42,20 @@ sample<-sample.split(imput,SplitRatio=0.8)
 train.set<-subset(imput, sample==TRUE)
 test.set <- subset(imput, sample==FALSE)
 
-
-#############################################################
-###################       KNN       #########################bufera
-#############################################################
-
 train.x <- train.set[,1:8]
 test.x <- test.set[, 1:8]
 train.y <- train.set[,9]
 test.y <- test.set[,9]
 
 classes <- factor(train.set$diabetes)
+
+
+##########################################################################################
+
+#                                     KNN  
+
+##########################################################################################
+
 
 predicted.classes <- knn(train.x, test.x, classes, k=3)
 
@@ -69,7 +76,6 @@ for (i in seq(along=ks)){
   
 }
 
-#x11()
 plot(misclass.train,xlab="Number of NN",ylab="Test error",type="n",xaxt="n",ylim=c(0,0.4)) #type="n" non disegna nulla, xaxt="n" non mette valori sull'asse x
 axis(1, 1:length(ks), as.character(ks)) 
 lines(misclass.test,type="b",col='blue',pch=20)   
@@ -78,9 +84,7 @@ legend("bottomright",lty=1,col=c("red","blue"),legend = c("train ", "test "))
 title('Test performace vs Train performace')
 
 
-###########################################################
-
-# Decision Boundaries
+# Decision Boundaries NO
 
 col1<-c('blue', 'magenta')
 
@@ -88,7 +92,7 @@ library(grid)
 plot.knn <- function(kneighbours){
   grid.x2 <- seq(min(imput[,2]), max(imput[,2]), 1)
   grid.x6 <- seq(min(imput[,6]), max(imput[,6]), 1)
-  grid <- expand.grid(mean(imput[,1]),grid.x2,mean(imput[,3]),mean(imput[,4]),mean(imput[,5]),grid.x6,mean(imput[,7]),mean(imput[,8]))
+  grid <- expand.grid(0,grid.x2,mean(imput[,3]),0,0,grid.x6,0,0)
   colnames(grid) <- colnames(train.x)
   predicted.classes <- knn(train.x, grid, classes, k=kneighbours)
   x11()
@@ -156,9 +160,11 @@ predicted.matrix <- matrix(pr.cl.val, length(grid.x2), length(grid.x6))
 contour(grid.x2, grid.x6, predicted.matrix, levels=c(1.5), drawlabels=FALSE,add=TRUE) # but it would be more reasonable draw a linear function
 
 
-####################################################
+##########################################################################################
 
-# PCA
+#                                          PCA
+
+###########################################################################################
 
 
 #install.packages("ggfortify")
@@ -192,9 +198,11 @@ plot.knn <- function(kneighbours){
 
 plot.knn(1)
 
-###########
+######################################################################################################
 
-# CART
+#                                              CART
+
+######################################################################################################
 
 install.packages('tidyverse')
 install.packages('caret')
